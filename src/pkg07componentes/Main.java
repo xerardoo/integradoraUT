addcc24b4058d5c0836f1b709e5748a7d75389fd
xer <xer@xer-lap.mshome.net>
2013-08-09 08:40:31 -0700

    ola k ase

---
+++ src/pkg07componentes/Main.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pkg07componentes;
+
+/**
+ *
+ * @author Tuti
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        frmPrincipal frm=new frmPrincipal();
+        frm.setVisible(true);
+    }
+}
